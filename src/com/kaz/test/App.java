be703d9e350a720ef28b7db2bf43690c028f4621
Rashed Rubby Riyadh <rashed.rubby@yahoo.com>
2015-01-22 16:45:47 +0600

    Added Support for Polygon to Select Warping Area

--- src/com/kaz/test/App.java
+++ src/com/kaz/test/App.java
@@ -7,6 +7,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.Polygon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -16,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFileChooser;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -67,39 +70,87 @@ class WarpImageTest extends JPanel {
 	private List<Point> nearestPoints;
 	private int radii = 100;
 	private int radius = radii / 2;
+	private JButton circleButton;
+	private JButton polygonButton;
+	private boolean isPolygon;
+	private boolean drawNow;
+	private List<Integer> xPoints;
+	private List<Integer> yPoints;
+	private Polygon polygon;
 
 	public WarpImageTest() {
 		try {
+			circleButton = new JButton("Circle");
+			polygonButton = new JButton("Polygon");
+			this.add(circleButton);
+			this.add(polygonButton);
 			inputImage = ImageIO.read(new File("D:/Image/image.jpg"));
 			outputImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
 			srcGrid = new WarpGrid(GRID_NUMBER, GRID_NUMBER, inputImage.getWidth(), inputImage.getHeight());
 			dstGrid = new WarpGrid(GRID_NUMBER, GRID_NUMBER, inputImage.getWidth(), inputImage.getHeight());
-			nearestPoints = new ArrayList<Point>();
 			warpImage(1, 1, srcGrid.xGrid[1], srcGrid.yGrid[1]);
-			// drawLines(dstGrid, Color.BLACK);
+			drawLines(dstGrid, Color.BLACK);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		circleButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent paramActionEvent) {
+				isPolygon = false;
+			}
+		});
+		polygonButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				isPolygon = true;
+			}
+		});
+
 		addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent paramMouseEvent) {
+			public void mouseClicked(MouseEvent event) {
 
 			}
 
 			@Override
 			public void mousePressed(MouseEvent e) {
-				warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
 				clickedPoint = new Point(e.getX(), e.getY());
-				Point circlePoint = new Point(e.getX() - radius, e.getY() - radius);
-				drawCircle(circlePoint, radii, Color.RED);
-//				drawLines(dstGrid, Color.BLUE);
-				repaint();
+				if (!isPolygon) {
+					warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
+					Point circlePoint = new Point(e.getX() - radius, e.getY() - radius);
+					drawCircle(circlePoint, radii, Color.WHITE);
+					drawLines(dstGrid, Color.BLACK);
+					repaint();
+				} else {
+					if (!drawNow) {
+						xPoints = new ArrayList<Integer>();
+						yPoints = new ArrayList<Integer>();
+						xPoints.add(clickedPoint.x);
+						yPoints.add(clickedPoint.y);
+					} else {
+
+					}
+				}
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
+				inputImage = outputImage;
+				outputImage = new BufferedImage(inputImage.getWidth(), inputImage.getHeight(),
+						BufferedImage.TYPE_INT_ARGB);
+				srcGrid = new WarpGrid(GRID_NUMBER, GRID_NUMBER, inputImage.getWidth(), inputImage.getHeight());
+				dstGrid = new WarpGrid(GRID_NUMBER, GRID_NUMBER, inputImage.getWidth(), inputImage.getHeight());
+				warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
+				drawLines(dstGrid, Color.BLACK);
+				if (isPolygon) {
+					if (!drawNow)
+						drawPolygon();
+					drawNow = !drawNow;
+				}
 				repaint();
 			}
 
@@ -107,32 +158,60 @@ class WarpImageTest extends JPanel {
 		addMouseMotionListener(new MouseAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				releasedPoint = new Point(e.getX(), e.getY());
-				Point circlePoint = new Point(e.getX() - radius, e.getY() - radius);
-				Point point = findIndex(clickedPoint.x, clickedPoint.y);
+				if (isPolygon) {
+					if (drawNow) {
+						releasedPoint = new Point(e.getX(), e.getY());
+						int deltaX = releasedPoint.x - clickedPoint.x;
+						int deltaY = releasedPoint.y - clickedPoint.y;
+						clickedPoint.x = releasedPoint.x;
+						clickedPoint.y = releasedPoint.y;
+						nearestPoints = new ArrayList<Point>();
+						findPointsInsidePolygon();
+						for (Point p : nearestPoints) {
+							dstGrid.xGrid[p.x] = dstGrid.xGrid[p.x] + deltaX;
+							dstGrid.yGrid[p.y] = dstGrid.yGrid[p.y] + deltaY;
+						}
+						warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
+						
+					} else {
+						releasedPoint = new Point(e.getX(), e.getY());
+						xPoints.add(releasedPoint.x);
+						yPoints.add(releasedPoint.y);
+						
+					}
+					drawPolygon();
+					drawLines(dstGrid, Color.BLACK);
+					repaint();
 
-				int deltaX = releasedPoint.x - clickedPoint.x;
-				int deltaY = releasedPoint.y - clickedPoint.y;
-				clickedPoint.x = releasedPoint.x;
-				clickedPoint.y = releasedPoint.y;
-				nearestPoints = new ArrayList<Point>();
-				findAllNearestPoints(point, radius / 2);
-				// System.out.println(nearestPoints.size());
-				for (Point p : nearestPoints) {
-					dstGrid.xGrid[p.x] = dstGrid.xGrid[p.x] + deltaX;
-					dstGrid.yGrid[p.y] = dstGrid.yGrid[p.y] + deltaY;
+				} else {
+					releasedPoint = new Point(e.getX(), e.getY());
+					Point circlePoint = new Point(e.getX() - radius, e.getY() - radius);
+					Point point = findIndex(clickedPoint.x, clickedPoint.y);
+
+					int deltaX = releasedPoint.x - clickedPoint.x;
+					int deltaY = releasedPoint.y - clickedPoint.y;
+					clickedPoint.x = releasedPoint.x;
+					clickedPoint.y = releasedPoint.y;
+					nearestPoints = new ArrayList<Point>();
+					findAllNearestPoints(point, radius);
+					for (Point p : nearestPoints) {
+						dstGrid.xGrid[p.x] = dstGrid.xGrid[p.x] + deltaX;
+						dstGrid.yGrid[p.y] = dstGrid.yGrid[p.y] + deltaY;
+					}
+					warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
+					drawLines(dstGrid, Color.BLACK);
+					drawCircle(circlePoint, radii, Color.WHITE);
+					repaint();
 				}
-				warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
-//				drawLines(dstGrid, Color.BLUE);
-				drawCircle(circlePoint, radii, Color.RED);
-				repaint();
 			}
 
 			@Override
 			public void mouseMoved(MouseEvent e) {
-//				warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
-//				drawCircle(new Point(e.getX() - radius, e.getY() - radius), radii, Color.RED);
-//				repaint();
+
+				// warpImage(1, 1, dstGrid.xGrid[1], dstGrid.yGrid[1]);
+				// drawCircle(new Point(e.getX() - radius, e.getY() - radius),
+				// radii, Color.RED);
+				// repaint();
 			}
 		});
 	}
@@ -156,6 +235,7 @@ class WarpImageTest extends JPanel {
 		dstGrid.xGrid[x] = xVal;
 		dstGrid.yGrid[y] = yVal;
 		warpFilter = new WarpFilter(srcGrid, dstGrid);
+		// warpFilter.setMorphImage(morphImage);
 		warpFilter.filter(inputImage, outputImage);
 	}
 
@@ -190,6 +270,23 @@ class WarpImageTest extends JPanel {
 
 	}
 
+	public void drawLine(Point p1, Point p2) {
+		graphics = (Graphics2D) outputImage.getGraphics();
+		graphics.setColor(Color.BLACK);
+		graphics.setStroke(new BasicStroke(1));
+		graphics.drawLine(p1.x, p1.y, p2.x, p2.y);
+	}
+
+	public void drawPolygon() {
+		graphics = (Graphics2D) outputImage.getGraphics();
+		graphics.setColor(Color.BLACK);
+		graphics.setStroke(new BasicStroke(1));
+		int x[] = xPoints.stream().mapToInt(i -> i).toArray();
+		int y[] = yPoints.stream().mapToInt(i -> i).toArray();
+
+		graphics.drawPolyline(x, y, xPoints.size());
+	}
+
 	public Point findIndex(int x, int y) {
 		Point point = new Point();
 		int minDistance = 214748364;
@@ -217,6 +314,17 @@ class WarpImageTest extends JPanel {
 						nearestPoints.add(new Point(x, x));
 					}
 				}
+			}
+		}
+	}
+
+	public void findPointsInsidePolygon() {
+		polygon = new Polygon(xPoints.stream().mapToInt(i -> i).toArray(), yPoints.stream().mapToInt(i -> i).toArray(),
+				xPoints.size());
+		for (int i = 0; i < GRID_NUMBER * GRID_NUMBER; i++) {
+			Point p = new Point((int) dstGrid.xGrid[i], (int) dstGrid.yGrid[i]);
+			if (polygon.contains(p)) {
+				nearestPoints.add(new Point(i, i));
 			}
 		}
 	}
